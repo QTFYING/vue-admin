64b99c66ae72533ca9ec6f9ff89dbb8fc601b17c
yapeng.ma <yapeng.ma@ly.com>
2025-03-26 13:47:51 +0800

    ci: 加入版本检测功能

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## 打包注意事项
 
 - 生成环境 `sudo pnpm build --mode production`
+- 生成版本文件 `EXTERNAL='修复了一些BUG' npm run generate:version`
--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
   import { ElConfigProvider } from 'element-plus';
   import zhCn from 'element-plus/es/locale/lang/zh-cn';
   import { defineComponent } from 'vue';
+  import { checkVersion } from 'version-rocket';
 
   export default defineComponent({
     name: 'app',
@@ -21,6 +22,21 @@
       currentLocale() {
         return zhCn;
       },
+    },
+    beforeCreate() {
+      const { version, name: title } = __APP_INFO__.pkg;
+      const { VITE_PUBLIC_PATH, MODE } = import.meta.env;
+      if (MODE === 'production') {
+        checkVersion(
+          {
+            pollingTime: 100 * 60 * 5, // 5分钟
+            localPackageVersion: version,
+            originVersionFileUrl: `${location.origin}${VITE_PUBLIC_PATH}version.json`,
+          },
+          // options
+          { title, description: '检测到新版本', buttonText: '立即更新' },
+        );
+      }
     },
   });
 </script>
